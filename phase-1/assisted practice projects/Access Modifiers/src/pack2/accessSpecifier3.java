b905df6e258610564f07b9bed1fd38e9668c253a
asadi-bharadwaj <97620217+asadi-bharadwaj@users.noreply.github.com>
2022-01-27 14:26:13 +0530

    Add files via upload

---
+++ phase-1/assisted practice projects/Access Modifiers/src/pack2/accessSpecifier3.java
@@ -0,0 +1,11 @@
+package pack2;
+import  pack1.*;
+public class accessSpecifier3 {
+public static void main(String[] args) {
+		
+		publicAccessSpecifier obj = new publicAccessSpecifier(); 
+      ;  
+		
+	}
+
+}
