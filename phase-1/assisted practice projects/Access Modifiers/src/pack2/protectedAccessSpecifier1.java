48909eb5c5a4c2d6019bb3180f0f888e0fdc58d0
asadi-bharadwaj <97620217+asadi-bharadwaj@users.noreply.github.com>
2022-01-27 17:35:41 +0530

    Add files via upload

---
+++ phase-1/assisted practice projects/Access Modifiers/src/pack2/protectedAccessSpecifier1.java
@@ -0,0 +1,11 @@
+package pack2;
+import pack1.*;
+public class protectedAccessSpecifier1  extends protectedAccessSpecifier   {
+	public static void main(String[] args) {
+		protectedAccessSpecifier1 obj = new protectedAccessSpecifier1 ();   
+	           
+	}
+
+	
+
+}
