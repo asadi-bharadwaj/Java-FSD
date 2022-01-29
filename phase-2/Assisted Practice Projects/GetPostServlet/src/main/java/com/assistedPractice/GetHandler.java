a5bc335d6cfb9c1a2460da74ba33c16b270fd66b
asadi-bharadwaj <97620217+asadi-bharadwaj@users.noreply.github.com>
2022-01-29 12:24:40 +0530

    Add files via upload

---
+++ phase-2/Assisted Practice Projects/GetPostServlet/src/main/java/com/assistedPractice/GetHandler.java
@@ -0,0 +1,22 @@
+package com.assistedPractice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GetHandler extends HttpServlet{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+		PrintWriter out=res.getWriter();
+		out.println("<html><body>");
+		String name=req.getParameter("name");
+		String address=req.getParameter("address");
+		out.println("name "+name+"address "+address);
+		out.println("</body></html>");
+		
+	}
+
+}
